8a356741b944a030f7a86091f8e1362806f092a7
sasaki <vofxx@icloud.com>
2020-10-27 09:23:06 +0000

    f

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,7 +8,6 @@ class CardController < ApplicationController
 
   def create 
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    #binding.pry
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -18,8 +17,8 @@ class CardController < ApplicationController
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
       ) 
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
+      card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if card.save
         redirect_to ("/card/show")
       else
         redirect_to action: "pay"
@@ -36,17 +35,17 @@ class CardController < ApplicationController
       customer.delete
       card.delete
     end
-      redirect_to action: "new"
+      redirect_to action: "show"
   end
   
   def show 
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new" 
+    @card = Card.where(user_id: current_user.id).first
+    #binding.pry
+    if @card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
   get 'card/new'
   post 'card/create'
   get 'card/show'
+  post 'card/delete'
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
