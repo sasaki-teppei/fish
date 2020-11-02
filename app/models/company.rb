a5755b559201bc529622f5bae64d1da7b7c0a9a2
sasaki <vofxx@icloud.com>
2020-11-02 10:06:54 +0000

    f

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,10 @@
 class Company < ApplicationRecord
-    #validates :company_name, {uniqueness: true}
-    #validates :company_name, {presence: true}
+    validates :company_name, {uniqueness: true}
+    validates :company_name, {presence: true}
     validates :email, {uniqueness: true}
-    #validates :email, format: { with:/\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/ }
-    #validates :address, {presence: true, uniqueness: true}
-    #validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/}
+    validates :email, format: { with:/\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/ }
+    validates :address, {presence: true, uniqueness: true}
+    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/}
     validates :email, {presence: true}
     validates :password, {presence: true}
     
